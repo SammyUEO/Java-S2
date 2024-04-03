41878a4a36d4a021efe0c82b408cd324a51ad7f6
Samuel Pop <samuel.pop@emanuel.ro>
2024-04-03 14:03:17 +0300

    punct

--- src/Punct.java
+++ src/Punct.java
@@ -1,4 +1,18 @@
 
 public class Punct {
+	private int s;
+	private int l;
+	
+	public Punct(int s, int l) {
+		this.s = s;
+		this.l = l;
+	}
 
+	public int getS() {
+		return s;
+	}
+
+	public int getL() {
+		return l;
+	}
 }
--- src/PunctColorat.java
+++ src/PunctColorat.java
@@ -1,4 +1,12 @@
+public class PunctColorat extends Punct {
+    private int r, g, b;
+    private String rgb;
 
-public class PunctColorat {
-
+    public PunctColorat(int s, int l, int r, int g, int b) {
+        super(s, l);
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+    }
 }
